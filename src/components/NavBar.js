595e242abd4f77110d2b5ff12ab2dc5c51448bef
DirtyKeyboard <andrewxx000222@icloud.com>
2023-03-13 16:12:01 -0500

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const hypers = links.map((el) => {
+    return <a key={el} href={'#'+el}>{el}</a>
+  })
+  return (
+  <nav>
+    {hypers}
+  </nav>
+  );
 }
 
 export default NavBar;
+
+//a href="#home">home</a>
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,15 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const projEls = projects.map(el => {
+    return <ProjectItem key={el.id} name={el.name} about={el.about} technologies={el.technologies} />
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projEls}
+      </div>
     </div>
   );
 }
